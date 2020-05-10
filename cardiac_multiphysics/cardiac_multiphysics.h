7b5e70e83142968259dc31970d60a2fad6abd7e2
Thomas Scrase <thomas.scrase@postgrad.manchester.ac.uk>
2020-05-10 10:51:12 +0100

    Added reworked conduction lib and removed redundant folders

--- cardiac_multiphysics/cardiac_multiphysics.h
+++
@@ -1,3 +0,0 @@
-// This file was generated automatically during the make process
-// and it will be remade automatically
-#include<cardiac_multiphysics/cardiac_tissue_mono_elements.h> 
--- multiphysics_tests/multiphysics_tests.h
+++
@@ -1,5 +0,0 @@
-// This file was generated automatically during the make process
-// and it will be remade automatically
-#include<multiphysics_tests/anisotropic_solid_with_vector_storage_elements.h> 
-#include<multiphysics_tests/monodomain_with_d_elements.h> 
-#include<multiphysics_tests/monodomain_with_cell_elements.h> 
