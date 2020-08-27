bdd02b340f8761ca5cdfa35cb1fdced4e4a3563b
Thomas Scrase <thomas.scrase@postgrad.manchester.ac.uk>
2020-08-27 16:45:22 +0100

    Broken commit. Everything still broken

--- toms_blood_elements/valve_elements.cc
+++
@@ -1,8 +0,0 @@
-#include "valve_elements.h"
-
-namespace oomph
-{
-	//By default all valves open and close fully
-	double Default_Valve_Element_Min_Effective_Area_Coefficient = 0.0;
-	double Default_Valve_Element_Min_Effective_Area_Coefficient = 1.0;
-}
